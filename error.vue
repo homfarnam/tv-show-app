ac3a2fae7b97d8cd7920600dc53fc6633998cf47
homfarnam <homfarnam@gmail.com>
2022-11-26 23:51:48 +0100

    Add error page

---
+++ error.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    error: {
+      type: Object as PropType<Error & { statusCode: string }>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const { error } = props
+
+    const handleClearError = () => {
+      clearError({ redirect: '/' })
+    }
+
+    return { error, handleClearError }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
+
+<template>
+  <div class="my-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">
+      {{ error?.statusCode }}
+    </p>
+    <p class="mt-7 text-7xl">Oooops!</p>
+    <p class="mt-7 text-2xl">
+      {{ error?.message }}
+    </p>
+
+    <button class="btn my-7" @click="handleClearError">Go Home...</button>
+  </div>
+</template>
